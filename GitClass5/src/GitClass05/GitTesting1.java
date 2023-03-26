7473ec30b2aed95dcb2f82f03f7487b251f559c2
JamesRK5 <nogamesjamesrealestate@gmail.com>
2023-03-26 14:05:34 -0400

    commit practice

---
+++ GitClass5/src/GitClass05/GitTesting1.java
@@ -0,0 +1,11 @@
+package GitClass05;
+
+public class GitTesting1 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+        System.out.println("We did it");
+    }
+}
